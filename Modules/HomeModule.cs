0a92c3a7440e0070fea3363bcc2c55f2c805f2dc
Caitlin Hines and Jiwon Kang <caitlin+jiwon@>
2017-03-02 09:28:30 -0800

    add routes for clearing all Categories and Recipes, routing back to homepage

--- Modules/HomeModule.cs
+++ Modules/HomeModule.cs
@@ -14,32 +14,42 @@ namespace RecipeBox
             };
 
             Get["/recipes"] = _ => {
-              List<Recipe> AllRecipes = Recipe.GetAll();
-              return View["recipes.cshtml", AllRecipes];
+                List<Recipe> AllRecipes = Recipe.GetAll();
+                return View["recipes.cshtml", AllRecipes];
             };
             Get["/categories"] = _ => {
-              List<Category> AllCategories = Category.GetAll();
-              return View["categories.cshtml", AllCategories];
+                List<Category> AllCategories = Category.GetAll();
+                return View["categories.cshtml", AllCategories];
             };
 
             Get["/categories/new"] = _ => {
-              return View["categories_form.cshtml"];
+                return View["categories_form.cshtml"];
             };
             Post["/categories/new"] = _ => {
-              Category newCategory = new Category(Request.Form["category-name"]);
-              newCategory.Save();
-              return View["categories.cshtml", Category.GetAll()];
+                Category newCategory = new Category(Request.Form["category-name"]);
+                newCategory.Save();
+                return View["categories.cshtml", Category.GetAll()];
             };
 
             Get["/recipes/new"] = _ => {
-              List<Category> AllCategories = Category.GetAll();
-              return View["recipes_form.cshtml", AllCategories];
+                List<Category> AllCategories = Category.GetAll();
+                return View["recipes_form.cshtml", AllCategories];
             };
             Post["/recipes/new"] = _ => {
-            Recipe newRecipe = new Recipe(Request.Form["recipe-name"], Request.Form["recipe-ingredient"], Request.Form["recipe-instruction"], Request.Form["recipe-rating"], Request.Form["category-id"]);
-            newRecipe.Save();
-            return View["recipes.cshtml"];
-          };
+                Recipe newRecipe = new Recipe(Request.Form["recipe-name"], Request.Form["recipe-ingredient"], Request.Form["recipe-instruction"], Request.Form["recipe-rating"], Request.Form["category-id"]);
+                newRecipe.Save();
+                return View["recipes.cshtml", Recipe.GetAll()];
+            };
+
+            Post["/categories/delete"] = _ => {
+                Category.DeleteAll();
+                Recipe.DeleteAll();
+                return View["index.cshtml"];
+            };
+            Post["/recipes/delete"] = _ => {
+                Recipe.DeleteAll();
+                return View["index.cshtml"];
+            };
         }
     }
 }
