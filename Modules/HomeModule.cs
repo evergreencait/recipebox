64f5506efa630e7ef422c1c5b557828929202017
Caitlin Hines and Jiwon Kang <caitlin+jiwon@>
2017-03-02 09:17:27 -0800

    add recipes_form.cshtml and categories_form.cshtml

--- Modules/HomeModule.cs
+++ Modules/HomeModule.cs
@@ -17,11 +17,29 @@ namespace RecipeBox
               List<Recipe> AllRecipes = Recipe.GetAll();
               return View["recipes.cshtml", AllRecipes];
             };
-
             Get["/categories"] = _ => {
               List<Category> AllCategories = Category.GetAll();
               return View["categories.cshtml", AllCategories];
             };
+
+            Get["/categories/new"] = _ => {
+              return View["categories_form.cshtml"];
+            };
+            Post["/categories/new"] = _ => {
+              Category newCategory = new Category(Request.Form["category-name"]);
+              newCategory.Save();
+              return View["categories.cshtml", Category.GetAll()];
+            };
+
+            Get["/recipes/new"] = _ => {
+              List<Category> AllCategories = Category.GetAll();
+              return View["recipes_form.cshtml", AllCategories];
+            };
+            Post["/recipes/new"] = _ => {
+            Recipe newRecipe = new Recipe(Request.Form["recipe-name"], Request.Form["recipe-ingredient"], Request.Form["recipe-instruction"], Request.Form["recipe-rating"], Request.Form["category-id"]);
+            newRecipe.Save();
+            return View["recipes.cshtml"];
+          };
         }
     }
 }
